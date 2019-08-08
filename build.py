468fd595f5def02b8011c305fcfb704eb14c2c2f
Baldur van Lew <baldurvanlew@gmail.com>
2019-08-08 14:00:53 +0200

    Pass an extra environment variable to the conan runner docker

--- build.py
+++ build.py
@@ -3,9 +3,11 @@
 
 
 from bincrafters import build_template_default
+import os
 
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
-
+    # pass access token to the docker conan runner
+    builder.add({"BLDRVNLW_TOKEN": os.environ["BLDRVNLW_TOKEN"]})
     builder.run()
