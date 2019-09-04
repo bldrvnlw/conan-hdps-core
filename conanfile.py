ffc87e59218ca04c925c7b8bcc4c9f5233699813
Baldur van Lew <baldurvanlew@gmail.com>
2019-09-04 18:23:19 +0200

    Try source subfolder for build trigger

--- conanfile.py
+++ conanfile.py
@@ -55,7 +55,7 @@ class HdpsCoreConan(ConanFile):
         source_url = self.url
         self.run("git clone {0}.git".format(self.validated_url))
         os.chdir("./{0}".format(self._source_subfolder))
-        commit_sha = self._get_commit_sha(os.path.join(self.this_dir, "build_trigger.json"))
+        commit_sha = self._get_commit_sha(os.path.join(self._source_subfolder, "build_trigger.json"))
         self.run("git checkout {0}".format(commit_sha))
         os.chdir("..")
         with open("core/CMakeLists.txt",'r') as viewFileOpen:
