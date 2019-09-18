af96fc6f0a81012706ce73e8303ba3f444b29028
Baldur van Lew <baldurvanlew@gmail.com>
2019-09-18 12:06:13 +0200

    Disable cppstd to see if build proceeds

--- build.py
+++ build.py
@@ -8,6 +8,6 @@ import os
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder() 
-    for b in builder.items:
-        b.settings["compiler.cppstd"] = 14    
+    #for b in builder.items:
+    #    b.settings["compiler.cppstd"] = 14    
     builder.run()
