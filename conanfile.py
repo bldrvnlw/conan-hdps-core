a0123a34df60b513a386f7b0a318a43975453d0f
Baldur van Lew <baldurvanlew@gmail.com>
2019-09-18 14:38:34 +0200

    correct the libgl package install

--- conanfile.py
+++ conanfile.py
@@ -44,7 +44,7 @@ class HdpsCoreConan(ConanFile):
             if tools.os_info.with_apt:
                 installer = tools.SystemPackageTool()
                 installer.install('mesa-common-dev')
-                installer.install('libGL')
+                installer.install('libgl1-mesa-dev')
                 
     def config_options(self):
         if self.settings.os == 'Windows':
