f9fbe925be0d1fd752d5b9a1a4ee4f714fa616a5
Baldur van Lew <baldurvanlew@gmail.com>
2019-09-18 15:37:43 +0200

    Use the Netscape Portable Runtime library

--- conanfile.py
+++ conanfile.py
@@ -50,8 +50,7 @@ class HdpsCoreConan(ConanFile):
                 installer.install('libxcursor-dev')
                 installer.install('libxi-dev')
                 installer.install('libnss3-dev')
-                installer.install('libplc4')
-                installer.install('libnspr4')
+                installer.install('libnspr4-dev')
                 installer.install('libfreetype_dev')
                 
     def config_options(self):
