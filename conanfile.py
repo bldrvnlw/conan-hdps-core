cf616ca7d7b5a54ed838510145c9f86f7ea31459
Baldur van Lew <baldurvanlew@gmail.com>
2019-09-20 09:16:19 +0200

    In macos add libomp-dev (is not available by default)

--- conanfile.py
+++ conanfile.py
@@ -57,7 +57,7 @@ class HdpsCoreConan(ConanFile):
                 installer.install('libasound2-dev')
                 installer.install('libdbus-1-dev')
         if tools.os_info.is_macos: 
-            installer.install('libomp')        
+            installer.install('libomp-dev')        
                 
     def config_options(self):
         if self.settings.os == 'Windows':
