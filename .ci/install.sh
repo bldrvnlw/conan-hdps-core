5c046e1106d0cc9d22cb5bc73a2466c284001c29
Baldur van Lew <baldurvanlew@gmail.com>
2019-10-02 09:44:05 +0200

    Rehash to make citools available

--- .ci/install.sh
+++ .ci/install.sh
@@ -24,4 +24,8 @@ pip install conan_package_tools bincrafters_package_tools
 pip install pygithub
 pip install git+https://github.com/bldrvnlw/citools.git#egg=citools
 
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    pyenv rehash
+fi
+
 conan user
