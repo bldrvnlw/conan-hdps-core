8b00a7fbe9a6cd94bdf949be9dc3a2d72e650834
Baldur van Lew <baldurvanlew@gmail.com>
2019-10-02 11:41:25 +0200

    Integrated the commit notification in the run where the python env is correct

--- .ci/run.sh
+++ .ci/run.sh
@@ -9,4 +9,5 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv activate conan
 fi
 
+python -c "from citools.githubtools import set_build_status, State; set_build_status('build_trigger.json', '$CONAN_BLDRVNLW_TOKEN', '$TRAVIS_BUILD_WEB_URL', 'Travis', State.pending)"
 python build.py
