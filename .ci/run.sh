70817ef7c0d246bf4de4b3b6e9953b2b66f9f6a7
bldrvnlw <baldurvanlew@gmail.com>
2020-06-05 10:54:28 +0200

    Only set CONAN_CMAKE_PROGRAM for macOS
    In linux this is installed in the docker context

--- .ci/run.sh
+++ .ci/run.sh
@@ -7,6 +7,8 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
         eval "$(pyenv init -)"
     fi
     pyenv activate conan
+    export CONAN_CMAKE_PROGRAM=`which cmake`
+    cmake --version
 fi
 
 python -c "from citools.githubtools import set_build_status, State; set_build_status('build_trigger.json', '$CONAN_BLDRVNLW_TOKEN', '$TRAVIS_BUILD_WEB_URL', 'Travis', State.pending)"
